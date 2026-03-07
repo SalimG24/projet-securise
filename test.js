dc5c46244efa648baaab0c8e4b8f32f29e7c27a1
SalimG24 <salim.pro2406@gmail.com>
2026-03-07 15:08:26 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-let query = "SELECT * FROM users WHERE email = '" + email + "'";
+const express = require('express');
+const app = express();
+
+app.get('/login', (req, res) => {
+    // ICI : la donnée vient de l'URL (source non fiable)
+    const email = req.query.email; 
+    
+    // L'injection SQL classique
+    const query = "SELECT * FROM users WHERE email = '" + email + "'";
+    
+    db.execute(query); 
+});
